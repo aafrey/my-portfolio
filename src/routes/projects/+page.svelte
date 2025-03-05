0fe37feabbf529eef3306cf692375ed064c63031
aafrey <aafrey@mit.edu>
2025-03-05 15:49:47 -0500

    removed old info from page.svelte

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,7 +4,19 @@
     import projects from "$lib/projects.json";
   </script>
 <pre>{ JSON.stringify(projects, null, "\t") }</pre>
-<!-- <h1>Projects { projects.length } </h1>
+<h1>{ projects.length } Projects</h1>
+<div class="projects">
+    {#each projects as p}
+        <article>
+            <h2>{p.title}</h2>
+            <img src={p.image} alt="Project image">
+            <p>{p.description}</p>
+        </article>
+    {/each}
+</div>
+
+<!-- 
+
 <html lang="en", class = "home"></html>
         <div class="projects">
     {#each projects as p}
