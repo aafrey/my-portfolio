b22984ace1d802ed81af0bd2b693230b59793797
aafrey <aafrey@mit.edu>
2025-03-05 16:08:49 -0500

    added featured projects to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,3 +2,9 @@
 <h1>Aigneis Frey</h1>
         <p>Hi! I'm an Aero/Astro student at MIT interested in autonomous systems, robotics, embedded systems, etc. I also love to run and ski! </p>
         <img src="images/skiing.jpeg" alt="Me skiing last year in Mammoth, CA!" />
+        <h1>Featured Projects</h1>
+        <div class="projects">
+            {#each projects.slice(0, 3) as p}
+            <Project data={p} hLevel="3" />
+            {/each}
+        </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,26 +5,8 @@
     import Project from "$lib/Project.svelte"; // Import the component
   </script>
 <h1>Featured Projects</h1>
-<!-- <div class="projects">
-    {#each projects as p}
-        <Project data={p} />
-    {/each}
-</div> -->
 <div class="projects">
     {#each projects.slice(0, 3) as p}
     <Project data={p} hLevel="3" />
     {/each}
 </div>
-
-<!-- 
-
-<html lang="en", class = "home"></html>
-        <div class="projects">
-    {#each projects as p}
-        <article>
-            <h2>{p.title}</h2>
-            <img src={p.image} alt="">
-            <p>{p.description}</p>
-        </article>
-        {/each}
-    </div> -->
