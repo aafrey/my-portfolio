f932dcd97396838ed5c5e77bae1e9e8aaa0c0f8e
aafrey <aafrey@mit.edu>
2025-03-12 13:29:11 -0400

    lab 6 updated nav

--- static/global.js
+++ static/global.js
@@ -23,62 +23,57 @@ function $$ (selector, context = document) {
 // DONE: Remove <nav> menu from all HTML pages!
 // DONE: Comment out the step 2 code in this file!
 
+// AAF 2025-03-12 commenting out navigation per lab 6
+
 // step 3.1
-let pages = [
-	{url: "", title: "Home"},
-	{url: "projects/", title: "Projects"},
-	{url: "contact/", title: "Contact"},
-    {url: "resume/", title: "Resume"},
-    {url: "https://github.mit.edu/aafrey", title: "Github"},
-    { url: "6C850_Assignments/A2/", title: "A2" },
-    { url: "6C850_Assignments/A3/", title: "A3" }
-];
 // let pages = [
 // 	{url: "", title: "Home"},
 // 	{url: "projects/", title: "Projects"},
 // 	{url: "contact/", title: "Contact"},
-// 	{url: "https://github.mit.edu/aafrey", title: "Github"},
 //     {url: "resume/", title: "Resume"},
+//     {url: "https://github.mit.edu/aafrey", title: "Github"},
+//     { url: "6C850_Assignments/A2/", title: "A2" },
+//     { url: "6C850_Assignments/A3/", title: "A3" }
 // ];
 
-let nav = document.createElement("nav");
-document.body.prepend(nav);
-// document.body.insertAdjacentElement("afterbegin", nav);
+// let nav = document.createElement("nav");
+// document.body.prepend(nav);
+// // document.body.insertAdjacentElement("afterbegin", nav);
 
-// TODO: Inside index.html (our home page), add a class="home" attribute to the <html lang="en"> element!
-const ARE_WE_HOME = document.documentElement.classList.contains("home");
-console.log(document.documentElement.classList);
+// // TODO: Inside index.html (our home page), add a class="home" attribute to the <html lang="en"> element!
+// const ARE_WE_HOME = document.documentElement.classList.contains("home");
+// console.log(document.documentElement.classList);
 
-for (let p of pages) {
-	let url = p.url;
-	let title = p.title;
+// for (let p of pages) {
+// 	let url = p.url;
+// 	let title = p.title;
 
-	// Create link and add it to nav 
-    if (!ARE_WE_HOME && !url.startsWith("http")) {
-		// url = "../" + url;
-        url = "/my-portfolio/" + url;
-	}
-    console.log(url, ARE_WE_HOME);
+// 	// Create link and add it to nav 
+//     if (!ARE_WE_HOME && !url.startsWith("http")) {
+// 		// url = "../" + url;
+//         url = "/my-portfolio/" + url;
+// 	}
+//     console.log(url, ARE_WE_HOME);
 
-	// nav.insertAdjacentHTML("beforeend", `<a href="${ url }">${ title }</a>` );
-    let a = document.createElement("a");
-    a.href = url;
-    a.textContent = title;
+// 	// nav.insertAdjacentHTML("beforeend", `<a href="${ url }">${ title }</a>` );
+//     let a = document.createElement("a");
+//     a.href = url;
+//     a.textContent = title;
 
-    if (a.host === location.host && a.pathname === location.pathname) {
-        a.classList.add("current");
-    }
+//     if (a.host === location.host && a.pathname === location.pathname) {
+//         a.classList.add("current");
+//     }
 
-    // Highlight the current page
-    a.classList.toggle("current", a.host === location.host && a.pathname === location.pathname);
+//     // Highlight the current page
+//     a.classList.toggle("current", a.host === location.host && a.pathname === location.pathname);
 
-    // Open external links in a new tab
-    if (url.startsWith("http")) {
-        a.target = "_blank";
-    }
+//     // Open external links in a new tab
+//     if (url.startsWith("http")) {
+//         a.target = "_blank";
+//     }
 
-    nav.append(a);
-}
+//     nav.append(a);
+// }
 
 
 // step 3.2
